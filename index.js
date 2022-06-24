22c354f5b998dd1298470c198e0d7214404d2b0b
naquomcid <108144133+naquomcid@users.noreply.github.com>
2022-06-24 21:34:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require("fs");
 const config = require('./config.json');
-const BOT_TOKEN = config.BOT_TOKEN;
 const PREFIX = config.PREFIX;
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
